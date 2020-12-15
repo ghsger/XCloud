55fdfa99e754c2430749b3bd5c05be0dd9e3da3a
zf233 <13784570379@163.com>
2020-12-15 20:06:02 +0800

    add XCloud App version permission

--- app/src/main/java/cn/zf233/xcloud/activity/ActivityHome.java
+++ app/src/main/java/cn/zf233/xcloud/activity/ActivityHome.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.view.Gravity;
